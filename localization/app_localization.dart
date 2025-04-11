59156f1b28cb79ab573779061ced565cd2e185cc
MaryRafaat <mary.rafaat02@gmail.com>
2025-04-12 00:37:28 +0200

    Delete localization directory

--- localization/app_localization.dart
+++
@@ -1,7 +0,0 @@
-import '../core/app_export.dart';
-import 'en/en_translation.dart';
-
-class AppLocalization extends Translations {
-  @override
-  Map<String, Map<String, String>> get keys => {'en': en};
-}
