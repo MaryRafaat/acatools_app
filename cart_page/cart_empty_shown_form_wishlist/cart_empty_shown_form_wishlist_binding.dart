80c50c3718b889bd45889800d682995d3ba0ebc3
MaryRafaat <mary.rafaat02@gmail.com>
2025-04-12 00:27:21 +0200

    Delete cart_page directory

--- cart_page/cart_empty_shown_form_wishlist/cart_empty_shown_form_wishlist_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:acatools_app/screens/cart_empty_shown_form_wishlist/cart_empty_shown_form_wishlist_controller.dart';
-import 'package:acatools_app/core/app_export.dart';
-
-class CartEmptyShownFormWishlistBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut(() => CartEmptyShownFormWishlistController());
-  }
-}
--- cart_page/cart_empty_shown_form_wishlist/cart_empty_shown_form_wishlist_controller.dart
+++
@@ -1,8 +0,0 @@
-import 'package:acatools_app/screens/model.dart';
-
-import 'package:acatools_app/core/app_export.dart';
-
-class CartEmptyShownFormWishlistController extends GetxController {
-  Rx<CartEmptyShownFromWishlistModel> cartEmptyShownFormWishlistModelObj =
-      CartEmptyShownFromWishlistModel().obs;
-}
