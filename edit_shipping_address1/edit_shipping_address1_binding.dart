b9b4d736e4b7213fd31a003b90627e300ac07d22
MaryRafaat <mary.rafaat02@gmail.com>
2025-04-12 00:38:02 +0200

    Delete edit_shipping_address1 directory

--- edit_shipping_address1/edit_shipping_address1_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:acatools_app/core/app_export.dart';
-import 'package:acatools_app/screens/edit_shipping_address1/edit_shipping_address1_controller.dart';
-
-class EditShippingAddress1Binding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut(() => EditShippingAddress1Controller());
-  }
-}
