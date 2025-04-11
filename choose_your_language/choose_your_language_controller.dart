ae2eb08bd310c69f9104b10a328410f34b2389ee
MaryRafaat <mary.rafaat02@gmail.com>
2025-04-12 00:35:53 +0200

    Delete choose_your_language directory

--- choose_your_language/choose_your_language_controller.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:acatools_app/core/app_export.dart';
-import 'package:acatools_app/screens/model.dart';
-
-class ChooseYourLanguageController extends GetxController {
-  Rx<ChooseYourLanguageModel> chooseYourLanguageModelObj =
-      ChooseYourLanguageModel().obs;
-  Rx<String> language = "".obs;
-}
