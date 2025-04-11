577766cbfbc3e5605064b77f638efb8515f0d27a
MaryRafaat <mary.rafaat02@gmail.com>
2025-04-12 00:26:54 +0200

    Delete app_navigation directory

--- app_navigation/app_navigation_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:acatools_app/core/app_export.dart';
-import 'package:acatools_app/screens/app_navigation/app_navigation_controller.dart';
-
-class AppNavigationBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut<AppNavigationController>(() => AppNavigationController());
-  }
-}
--- app_navigation/app_navigation_controller.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:acatools_app/core/app_export.dart';
-import 'package:acatools_app/screens/model.dart';
-
-class AppNavigationController extends GetxController {
-  Rx<AppNavigatorModel> appNavigationModelObj = AppNavigatorModel().obs;
-}
