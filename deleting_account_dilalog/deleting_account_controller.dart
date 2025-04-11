78e4443ebd384bbbc86a769bc5760b4c47b875c4
MaryRafaat <mary.rafaat02@gmail.com>
2025-04-12 00:39:05 +0200

    Delete deleting_account_dilalog directory

--- deleting_account_dilalog/deleting_account_controller.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:acatools_app/core/app_export.dart';
-import 'package:acatools_app/screens/model.dart';
-
-class DeletingAccountController extends GetxController {
-  Rx<DeletingAccountModel> deletingAccountModelObj = DeletingAccountModel().obs;
-}
