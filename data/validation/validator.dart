523a4e8119517743204d4998808e31c371f0af32
MaryRafaat <mary.rafaat02@gmail.com>
2025-04-12 00:39:20 +0200

    Delete data directory

--- data/validation/validator.dart
+++
@@ -1,9 +0,0 @@
-import 'package:email_validator/email_validator.dart';
-
-class Validator {
-  static validateEmail(emailController) {
-    return EmailValidator.validate(emailController.text)
-        ? null
-        : 'Invalid email';
-  }
-}
