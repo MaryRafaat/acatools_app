2b8d0d8031cb7ebfdc687ae9392c45ef1306d01e
MaryRafaat <mary.rafaat02@gmail.com>
2025-04-12 00:35:24 +0200

    Delete choose_your_currency directory

--- choose_your_currency/choose_your_currency_controoler.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:acatools_app/core/app_export.dart';
-import 'package:acatools_app/screens/model.dart';
-
-class ChooseYourCurrencyController extends GetxController {
-  Rx<ChooseYourCurrencyModel> chooseYourCurrencyModelObj =
-      ChooseYourCurrencyModel().obs;
-  Rx<String> radioGroup = "".obs;
-}
