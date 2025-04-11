cb9d8bc1e92aabc91dfb8cbddb8eb355c8d1ab23
MaryRafaat <mary.rafaat02@gmail.com>
2025-04-12 00:37:12 +0200

    Delete flash_sale directory

--- flash_sale/flash_sale_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:acatools_app/core/app_export.dart';
-import 'package:acatools_app/screens/flash_sale/flash_sale_controller.dart';
-
-class FlashSaleBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut(() => FlashSaleController());
-  }
-}
--- flash_sale/flash_sale_controller.dart
+++
@@ -1,6 +0,0 @@
-import 'package:acatools_app/core/app_export.dart';
-import 'package:acatools_app/screens/model.dart';
-
-class FlashSaleController extends GetxController {
-  Rx<FlashSaleModel> flashSaleModelObj = FlashSaleModel().obs;
-}
